db0ba386474bd13d128efe494c843d175ebaa228
Andrew Lyons <andrew.r.lyons99@gmail.com>
2026-05-07 14:05:39 -0400

    set retention period of CW logs

---
+++ cloudwatch.tf
@@ -0,0 +1,8 @@
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/${var.project_name}"
+  retention_in_days = var.log_retention_days
+
+  tags = {
+    Project = var.project_name
+  }
+}
